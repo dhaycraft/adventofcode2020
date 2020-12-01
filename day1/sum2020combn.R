e2575532eb3e8d1213defea4ac7ca02e6d0d80ce
dhaycraft <haycr009@gmail.com>
2020-12-01 00:38:45 -0600

    Update sum2020combn.R
    change pathing

--- day1/sum2020combn.R
+++ day1/sum2020combn.R
@@ -1,6 +1,6 @@
 # Find the two entries that sum to 2020; what do you get if you multiply them together?
 
-input_numbers=read.table("C:/Users/haycr/Desktop/Repositories/adventofcode2020/day1/input_numbers.txt", quote="\"", comment.char="")
+input_numbers=read.table("day1/input_numbers.txt", quote="\"", comment.char="")
 
 get_n_sum_2020 <- function(input, pairs){
   all_combn <- data.frame(t(combn(input,pairs)))
